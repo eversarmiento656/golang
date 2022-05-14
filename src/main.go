4dfa5edabd7897f02310895f77cc9020ff148092
ever <eversarmiento656@gmail.com>
2022-05-14 10:44:30 -0500

    :sparkles: punteros

--- src/main.go
+++ src/main.go
@@ -7,6 +7,12 @@ import (
 
 const HOLA = "Saludo"
 
+type pc struct {
+	ram   int
+	disk  int
+	marca string
+}
+
 func main() {
 	var myCar mypackage.CarPublic
 	myCar.Brand = "Ferrari"
@@ -14,4 +20,37 @@ func main() {
 	fmt.Println(myCar)
 
 	mypackage.PrintMessage(HOLA)
+
+	//punteros
+	a := 50
+	b := &a
+
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{
+		ram:   6,
+		disk:  200,
+		marca: "msi",
+	}
+
+	fmt.Println(myPc)
+	myPc.ping()
+	myPc.duplicateRam()
+	fmt.Println(myPc)
+	myPc.duplicateRam()
+	fmt.Println(myPc)
+	myPc.duplicateRam()
+	fmt.Println(myPc)
+}
+
+func (myPC pc) ping() {
+	fmt.Println(myPC.marca, "Pong")
+}
+
+func (myPC *pc) duplicateRam() {
+	myPC.ram = myPC.ram * 2
 }
