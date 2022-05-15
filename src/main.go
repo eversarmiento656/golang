b151523eb97b0d4f75dab86883927cc9deef1eff
ever <eversarmiento656@gmail.com>
2022-05-14 21:36:01 -0500

    :sparkles: channels

--- src/main.go
+++ src/main.go
@@ -6,15 +6,24 @@ import (
 )
 
 func main() {
-	var wg sync.WaitGroup
+	/*var wg sync.WaitGroup
 	fmt.Println("Hello")
 	wg.Add(1)
 	go say("world", &wg)
-	wg.Wait()
+	wg.Wait()*/
 
+	//channel
+	c := make(chan string, 1)
+	fmt.Println("Hello")
+	go say2("Bye", c)
+	fmt.Println(<-c)
 }
 
 func say(message string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println(message)
+}
+
+func say2(message string, c chan<- string) {
+	c <- message
 }
